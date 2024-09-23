8c69e77f3b00854ad6a36916a5a3760e743a7508
Andrew Manteza <manteza14@gmail.com>
2024-09-23 21:51:44 +0800

    added a separate thing for graphicdesign projects

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -10,9 +10,15 @@ export default class extends AbstractView {
         return `
             <h1>Hello There, I am Andrew</h1>
             <p>
-               This Website here is serves as my portfolio website made with native Javascript
+               This Website here serves as my portfolio website made with native Javascript
 
-               (I will make this prettier in the future)
+               (I will make this prettier in the future ;))
+
+
+               Features to be added:
+               1. Make it better for mobile
+               2. Add an admin function so the content aint hardcoded
+               3. Make it prettier
             </p>
             <p>
                 
--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -8,8 +8,8 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Posts</h1>
-            <p>You are viewing the posts!</p>
+            <h1>Graphic Design Projects</h1>
+            
         `;
     }
 }
