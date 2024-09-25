80b4d89bc27e7d6e68998a900624f0039c5092a6
Andrew Manteza <manteza14@gmail.com>
2024-09-25 17:48:31 +0800

    updated the description for images

--- frontend/static/js/views/Posts.js
+++ frontend/static/js/views/Posts.js
@@ -22,10 +22,10 @@ export default class extends AbstractView {
   <div class="gallery">
 
     <a target="_blank" href="img_5terre.jpg">
-      <img src="https://i.ibb.co/rMhnDj3/1.jpg alt="Cinque Terre" width="600" height="400">
+      <img src="https://i.ibb.co/rMhnDj3/1.jpg alt="Apex_Promo" width="600" height="400">
     </a>
     
-    <div class="desc">Add a description of the image here</div>
+    <div class="desc">I made a promo for Apex Rentals. I changed some of the details to ensure privacy</div>
   </div>
 </div>
 
@@ -33,27 +33,27 @@ export default class extends AbstractView {
 <div class="responsive">
   <div class="gallery">
     <a target="_blank" href="img_forest.jpg">
-      <img src="https://i.ibb.co/JF5NKsk/2.jpg"  alt="Forest" width="600" height="400">
+      <img src="https://i.ibb.co/JF5NKsk/2.jpg"  alt="Apex_FunFact" width="600" height="400">
     </a>
-    <div class="desc">Add a description of the image here</div>
+    <div class="desc">Here is a fun fact graphic I made for Apex Rentals</div>
   </div>
 </div>
 
 <div class="responsive">
   <div class="gallery">
     <a target="_blank" href="img_lights.jpg">
-      <img src="https://i.ibb.co/g6Hcvf5/4.jpg alt="Northern Lights" width="600" height="400">
+      <img src="https://i.ibb.co/g6Hcvf5/4.jpg alt="Apex_Review" width="600" height="400">
     </a>
-    <div class="desc">Add a description of the image here</div>
+    <div class="desc">Here is a client review for Apex Rentals with a name change for anonymity</div>
   </div>
 </div>
 
 <div class="responsive">
   <div class="gallery">
     <a target="_blank" href="img_mountains.jpg">
-      <img src="https://i.ibb.co/PWdpDrK/3.jpg" alt="Mountains" width="600" height="400">
+      <img src="https://i.ibb.co/PWdpDrK/3.jpg" alt="Apex_Poll" width="600" height="400">
     </a>
-    <div class="desc">Add a description of the image here</div>
+    <div class="desc">This is an example of a poll I made for Apex Rentals</div>
   </div>
 </div>
 
